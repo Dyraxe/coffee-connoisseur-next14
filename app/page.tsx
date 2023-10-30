8948646887aaa91284bdced10e39d4f742ef9589
Paulo Amorim <phdja25@gmail.com>
2023-10-30 07:41:18 -0300

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import styles from "./Home.module.css";
+export default function Home() {
+  return (
+    <>
+      <main className={styles.main}>
+        <h1>Coffee Connoisseur</h1>
+      </main>
+      <footer></footer>
+    </>
+  );
+}
